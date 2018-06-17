fe247a31f65d0b1b756d94564772d5ed6a08dd50
thetutlage <virk.officials@gmail.com>
2018-06-17 11:43:17 +0530

    refactor(init): add services prompt

--- init/index.js
+++ init/index.js
@@ -30,16 +30,50 @@ const license = {
   name: 'license'
 }
 
+const services = {
+  type: 'checkbox',
+  message: 'Select the CI services you are planning to use',
+  choices: [
+    {
+      name: 'Travis',
+      value: 'travis'
+    },
+    {
+      name: 'Appveyor',
+      value: 'appveyor'
+    },
+    {
+      name: 'Coveralls',
+      value: 'coveralls'
+    }
+  ],
+  name: 'services'
+}
+
+const appveyorUsername = {
+  type: 'input',
+  message: 'Enter appveyor username',
+  when: function (answers) {
+    return answers.services.indexOf('appveyor') > -1
+  },
+  name: 'appveyorUsername'
+}
+
 async function task () {
-  const answers = await inquirer.prompt([projectLang, isCore, license])
+  const answers = await inquirer.prompt([projectLang, isCore, license, services, appveyorUsername])
 
   const file = json('config.json')
 
   file.set({
     core: answers.core,
     ts: answers.lang === 'Typescript',
-    license: answers.license
+    license: answers.license,
+    services: answers.services
   })
+
+  if (answers.appveyorUsername) {
+    file.merge({ appveyorUsername: answers.appveyorUsername })
+  }
 
   file.save()
 }
