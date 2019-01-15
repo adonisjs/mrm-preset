2df5606e1b31c0630ab73e862d8eedd83592a9e1
thetutlage <virk.officials@gmail.com>
2019-01-15 15:32:06 +0530

    feat(readme-toc): add new task to generate readme file toc

---
+++ readme-toc/index.js
@@ -0,0 +1,36 @@
+/*
+* @adonisjs/mrm-preset
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+const { packageJson, install, template } = require('mrm-core')
+const { join } = require('path')
+
+function task () {
+  const pkgFile = packageJson()
+
+  /**
+   * Add git hook to re-generate the TOC
+   */
+  pkgFile.set(
+    'gitHooks.pre-commit',
+    `doctoc README.md --title='## Table of contents' && git add README.md`
+  )
+
+  /**
+   * Save the package file
+   */
+  pkgFile.save()
+
+  /**
+   * Install required dependencies
+   */
+  install(['doctoc'])
+}
+
+task.description = 'Generate TOC for readme.md file'
+module.exports = task
