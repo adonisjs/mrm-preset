6b0d511f62168ddb7fcdf6682d3c6ed36695fb92
Harminder virk <virk.officials@gmail.com>
2020-07-05 17:32:07 +0530

    fix: audit report should commit the html file

--- audit/index.js
+++ audit/index.js
@@ -12,14 +12,16 @@ const { packageJson, install } = require('mrm-core')
 function task () {
   const pkgFile = packageJson()
 
+  const auditHook = 'npm audit --production --json | ./node_modules/.bin/npm-audit-html && git add npm-audit.html'
+
   /**
    * Add git hook to generate npm audit report
    */
   let preCommit = pkgFile.get('husky.hooks.pre-commit')
   if (preCommit) {
-    preCommit = `${preCommit} && npm audit --production --json | ./node_modules/.bin/npm-audit-html`
+    preCommit = `${preCommit} && ${auditHook}`
   } else {
-    preCommit = 'npm audit --production --json | ./node_modules/.bin/npm-audit-html'
+    preCommit = auditHook
   }
   pkgFile.set('husky.hooks.pre-commit', preCommit)
 
