ec9e428a7349c06ece179ced57dd0d9f68c0b092
Harminder Virk <virk.officials@gmail.com>
2021-05-17 17:45:38 +0530

    refactor: update setup for husky v6

--- readme-toc/index.js
+++ readme-toc/index.js
@@ -7,29 +7,42 @@
 * file that was distributed with this source code.
 */
 
-const { packageJson, install } = require('mrm-core')
+const { join } = require('path')
+const { chmodSync } = require('fs')
+const { execSync } = require('child_process')
+const { packageJson, install, lines } = require('mrm-core')
 
 function task () {
+  /**
+   * Remove existing pre commit hook
+   */
   const pkgFile = packageJson()
-  const hook = 'doctoc README.md --title=\'## Table of contents\' && git add README.md'
-
-  /**
-   * Add git hook to re-generate the TOC
-   */
-  let preCommit = pkgFile.get('husky.hooks.pre-commit')
-
-  if (!preCommit) {
-    preCommit = hook
-  } else if (!preCommit.includes(hook)) {
-    preCommit = `${preCommit} && ${hook}`
-  }
-
-  pkgFile.set('husky.hooks.pre-commit', preCommit)
-
-  /**
-   * Save the package file
-   */
+  pkgFile.unset('husky.hooks.pre-commit')
   pkgFile.save()
+
+  /**
+   * Setup husky
+   */
+  execSync('npx husky install')
+
+  /**
+   * Create pre-commit file
+   */
+  const commitFile = lines('.husky/pre-commit')
+    .add('#!/bin/sh')
+    .add('. "$(dirname "$0")/_/husky.sh"')
+    .add('npx doctoc README.md --title=\'## Table of contents\'')
+    .add('git add README.md')
+
+  const fileAlreadyExists = commitFile.exists()
+  commitFile.save()
+
+  /**
+   * Change mode when file not already exists
+   */
+  if (!fileAlreadyExists) {
+    chmodSync(join(process.cwd(), '.husky/pre-commit'), 0o0755)
+  }
 
   /**
    * Install required dependencies
